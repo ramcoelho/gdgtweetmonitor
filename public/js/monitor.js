9e5e49253397938b674208b5c05c13b9e67f7070
helio <insign@gmail.com>
2014-10-29 12:59:34 -0300

    material design cards theme

--- public/js/monitor.js
+++ public/js/monitor.js
@@ -1,35 +1,58 @@
-$(function () {
-  var last_tweet = '0';
-  var production_url = 'http://localhost/gdgtweetmonitor/devfestnorte/';
-  var development_url = 'http://localhost/gdgtweetmonitor/example.json';
+var tweets = false;
+var last_tweet = '0';
+//var production_url = 'http://localhost/gdgtweetmonitor/devfestnorte/';
+var production_url = 'http://tm.app/férias/'; // Hélio/
+var development_url = 'http://localhost/gdgtweetmonitor/example.json';
 
-  setInterval(function() {
-    //var url = development_url;
-    var url = production_url + last_tweet;
-    var jqxhr = $.getJSON(url, function(data) {
-      var newrow;
-      if (data) {
-        $.each(data, function(key, val) {
-          last_tweet = val.id;
-          newrow = $('#template').clone();
-          newrow.attr('id', 'row-' + val.id);
-          var tweet = newrow.children('.tweet');
-          tweet.attr('id', val.id);
-          var autor = tweet.children('.author');
-          tweet.children('.text').text(val.text);
-          tweet.children('.timestamp').text(val.timestamp);
-          autor.children('.name').text(val.author.name);
-          autor.children('.screen-name').text(val.author.screen_name);
-          autor.children('.picture').children('img').attr('src', val.author.picture);
-          newrow.prependTo("#stage");
-          newrow.show();
-        });
-      }
-      data = false;
-    }).fail(function() {
-      console.log("Erro ao atualizar");
-    });
-  }, 10000);
+jQuery(document).ready(function ($) {
+  setInterval(function () {
+    busca_tweets()
+  }, 13000); // 10 + (3 para carregar as imagens)
   $('#template').hide();
+
+  // Começa imediatamente
+  busca_tweets();
 });
 
+
+function busca_tweets() {
+  //var url = development_url;
+  var url = production_url + last_tweet;
+  var jqxhr = $.getJSON(url, function (data) {
+    tweets = data;
+
+    $.each(tweets, function (key, val) { // Precarrega imagens de perfil
+      $('<img />').attr('src', val.author.picture).appendTo('.preload');
+    });
+
+    setTimeout(function () { // Espera um pouco ate as imagens carregarem
+      carrega_tweets()
+    }, 3000);
+    
+  }).fail(function () {
+    console.log("Erro ao atualizar");
+  });
+}
+
+function carrega_tweets() {
+  var newrow;
+  if (tweets) {
+    $.each(tweets, function (key, val) {
+      last_tweet = val.id;
+      newrow = $('#template').clone();
+      newrow.attr('id', 'row-' + val.id);
+      var tweet = newrow.children('.tweet');
+      tweet.attr('id', val.id);
+      var autor = tweet.children('.author');
+      autor.children('.text').text(val.text);
+      autor.children('.timestamp').text(val.timestamp);
+      autor.children('.name').text(val.author.name)
+      autor.children('.screen-name').text('@' + val.author.screen_name);
+      tweet.children('.picture').css('background-image', 'url(' + val.author.picture + ')');
+      newrow.prependTo("#stage");
+      newrow.fadeIn(800).show();
+    });
+
+  }
+  tweets = false;
+}
