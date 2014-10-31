851952b1c005991094444b6b3675bbc54cce396d
evaldobarbosa <evaldobarbosa@gmail.com>
2014-10-31 19:10:32 -0300

    modificações visuais

--- public/js/monitor.js
+++ public/js/monitor.js
@@ -1,26 +1,28 @@
 $(function () {
   var last_tweet = '0';
   var production_url = 'http://localhost/gdgtweetmonitor/devfestnorte/';
-  var development_url = 'http://localhost/gdgtweetmonitor/example.json';
+  var development_url = 'http://www.twitterdevfest.dev/gdgtweetmonitor/public/example.json';
 
   setInterval(function() {
-    //var url = development_url;
-    var url = production_url + last_tweet;
+    var url = development_url;
+    //var url = production_url + last_tweet;
     var jqxhr = $.getJSON(url, function(data) {
       var newrow;
       if (data) {
         $.each(data, function(key, val) {
           last_tweet = val.id;
           newrow = $('#template').clone();
-          newrow.attr('id', 'row-' + val.id);
+            newrow.attr('id', 'row-' + val.id);
+
           var tweet = newrow.children('.tweet');
-          tweet.attr('id', val.id);
+            tweet.attr('id', val.id);
+
           var autor = tweet.children('.author');
-          tweet.children('.text').text(val.text);
-          tweet.children('.timestamp').text(val.timestamp);
-          autor.children('.name').text(val.author.name);
-          autor.children('.screen-name').text(val.author.screen_name);
-          autor.children('.picture').children('img').attr('src', val.author.picture);
+            autor.children('.row-content').children('.lead').html(val.text);
+            autor.children('.row-content').children('.timestamp').text(val.timestamp);
+            autor.children('.row-content').children('.name').text(val.author.name);
+            autor.children('.row-content').children('.screen-name').text('@'+val.author.screen_name);
+            autor.children('.picture').children('img').attr('src', val.author.picture);
           newrow.prependTo("#stage");
           newrow.show();
         });
